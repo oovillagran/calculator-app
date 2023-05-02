9e4b4f07ac349f499ebf9f0cb275835f67283107
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-02 18:52:23 -0500

    Add styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Calculator from './components/Calculator';
 const App = () => (
   <>
     <div className="App">
-      <h1>Math Magicians</h1>
+      <h1 className="app-title">Math Magicians</h1>
     </div>
 
     <Calculator />
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,31 +8,31 @@ const Calculator = () => (
 function ChildComponent() {
   return (
     <div className="main-container">
-      <input className="input-screen" type="text" id="input" name="name" pattern="[0-9+\-*/()\s]+" />
+      <input className="input-screen height" type="text" id="input" name="name" pattern="[0-9+\-*/()\s]+" placeholder="0" />
       <div className="buttons-container">
-        <button className="button math-operation" type="button">AC</button>
-        <button className="button math-operation" type="button">+/-</button>
-        <button className="button math-operation" type="button">%</button>
-        <button className="button math-operation orange" type="button">+</button>
+        <button className="button math-operation height" type="button">AC</button>
+        <button className="button math-operation height" type="button">+/-</button>
+        <button className="button math-operation height" type="button">%</button>
+        <button className="button math-operation orange height" type="button">+</button>
         <br />
-        <button className="button math-number" type="button">7</button>
-        <button className="button math-number" type="button">8</button>
-        <button className="button math-number" type="button">9</button>
-        <button className="button math-operation orange" type="button">x</button>
+        <button className="button math-number height" type="button">7</button>
+        <button className="button math-number height" type="button">8</button>
+        <button className="button math-number height" type="button">9</button>
+        <button className="button math-operation orange height" type="button">x</button>
         <br />
-        <button className="button math-number" type="button">4</button>
-        <button className="button math-number" type="button">5</button>
-        <button className="button math-number" type="button">6</button>
-        <button className="button math-operation orange" type="button">-</button>
+        <button className="button math-number height" type="button">4</button>
+        <button className="button math-number height" type="button">5</button>
+        <button className="button math-number height" type="button">6</button>
+        <button className="button math-operation orange height" type="button">-</button>
         <br />
-        <button className="button math-number" type="button">1</button>
-        <button className="button math-number" type="button">2</button>
-        <button className="button math-number" type="button">3</button>
-        <button className="button math-operation orange" type="button">+</button>
+        <button className="button math-number height" type="button">1</button>
+        <button className="button math-number height" type="button">2</button>
+        <button className="button math-number height" type="button">3</button>
+        <button className="button math-operation orange height" type="button">+</button>
         <br />
-        <button className="button math-number" type="button">0</button>
-        <button className="button math-number" type="button">.</button>
-        <button className="button math-operation orange" type="button">=</button>
+        <button className="button-zero math-number height" type="button">0</button>
+        <button className="button math-number height" type="button">.</button>
+        <button className="button math-operation orange height" type="button">=</button>
       </div>
     </div>
   );
