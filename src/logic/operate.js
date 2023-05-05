ea1f31ebfcbdf64db42acda54bf1352bec13b84c
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-04 23:08:31 -0500

    Add changes in the Calculator.js and operate.js to meet optional requirements from CR

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,7 +1,7 @@
 import Big from 'big.js';
 
 export default function operate(numberOne, numberTwo, operation) {
-  const one = Big(numberOne);
+  const one = numberOne ? Big(numberOne) : Big(0);
   const two = Big(numberTwo);
   if (operation === '+') {
     return one.plus(two).toString();
