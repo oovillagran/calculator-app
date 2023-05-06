04a57eb7cccab0c59464821df069ca589fdd3a39
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-05 22:38:17 -0500

    Add Quote.js

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div className="quote-section">
+      <div className="quote-container">
+        <p>Quote</p>
+      </div>
+    </div>
+  );
+}
+
+export default Quote;
