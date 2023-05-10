b2d3b474f0d3cba84ef0129743a30669da2f8306
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-09 22:32:28 -0500

    Add styles to nav-bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,27 @@
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../styles/Navigation.css';
 
 export default function Navigation() {
   return (
     <>
-      <nav className="nav-bar">
-        <ul className="nav-container">
-          <li className="nav-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/calculator">Calculator</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/quote">Quote</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar bg="dark" variant="dark" className="nav-bar">
+        <Container>
+          <Nav className="me-auto nav-container justify-content-end">
+            <Nav.Link>
+              <Link className="nav-item" to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="nav-item" to="/calculator">Calculator</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="nav-item" to="/quote">Quote</Link>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
